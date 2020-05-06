0449bd88b3ec1676ee71b8dc3ceb78902537b32f
weiran92 <weiran92@xdf.cn>
2020-05-06 13:58:42 +0800

    test02

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+/*
+ * @Author       : weiran92@xdf.cn
+ * @Date         : 2020-05-06 13:56:57
+ * @LastEditors  : weiran92@xdf.cn
+ * @LastEditTime : 2020-05-06 13:58:27
+ * @Description  : 描述信息
+ */
 import OnOff from '@/components/onoff/index.js'
-
+// 2
 const components = [
     OnOff
 ]
